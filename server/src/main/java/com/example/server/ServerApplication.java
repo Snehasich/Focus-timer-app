b5f6f6b4f26454619d39b3edc934680fdc21bfb3
Snehasich <snehasich2005@gmail.com>
2026-04-05 20:23:20 +0530

    url changed in backend

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @SpringBootApplication
 public class ServerApplication {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
--- server/src/main/java/com/example/server/controller/TaskController.java
+++ server/src/main/java/com/example/server/controller/TaskController.java
@@ -11,7 +11,7 @@ import java.util.*;
 
 @RequestMapping("/tasks")
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class TaskController {
     
     private final TaskService service;
